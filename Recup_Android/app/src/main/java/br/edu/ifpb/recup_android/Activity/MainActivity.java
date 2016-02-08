d4176c110ac63ca8a95fc84cb2917b319701f6b0
rafaelbelarmino <rafaelon@live.com>
2016-02-08 18:55:06 -0300

    i

--- Recup_Android/app/src/main/java/br/edu/ifpb/recup_android/Activity/MainActivity.java
+++ Recup_Android/app/src/main/java/br/edu/ifpb/recup_android/Activity/MainActivity.java
@@ -1,13 +1,15 @@
-package br.edu.ifpb.recup_android;
+package br.edu.ifpb.recup_android.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import br.edu.ifpb.recup_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
